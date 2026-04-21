29fd64d9eb7968f8c0fb93a83046b7be37962821
anhtinhstinhs-gif <anhtinhstinhs@gmail.com>
2026-04-22 03:06:45 +0700

    Create scrip.lua

---
+++ scrip.lua
@@ -0,0 +1,57 @@
+F1::
+Send 1
+Sleep 300
+Send 1
+Sleep 300
+Send 2
+Sleep 600
+Send 1
+Sleep 600
+Send 4
+Sleep 600
+Send 3
+Sleep 1200
+
+Send 1
+Sleep 300
+Send 1
+Sleep 300
+Send 2
+Sleep 600
+Send 1
+Sleep 600
+Send 5
+Sleep 600
+Send 4
+Sleep 1200
+
+Send 1
+Sleep 300
+Send 1
+Sleep 300
+Send 8
+Sleep 600
+Send 6
+Sleep 600
+Send 4
+Sleep 600
+Send 3
+Sleep 600
+Send 2
+Sleep 1200
+
+Send 7
+Sleep 300
+Send 7
+Sleep 300
+Send 6
+Sleep 600
+Send 4
+Sleep 600
+Send 5
+Sleep 600
+Send 4
+Sleep 1500
+return
+
+F2::Pause
